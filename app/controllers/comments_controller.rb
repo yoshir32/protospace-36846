d43b4323b3d9b011d85ee3bcd9d78c3feef173f3
yoshir32 <yoshir0203@ezweb.ne.jp>
2022-02-01 00:43:55 +0900

    コメント空白で登録した時の動作修正

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,6 +6,8 @@ class CommentsController < ApplicationController
       redirect_to prototype_path(@comment.prototype)
     else
       @prototype = @comment.prototype
+      @comment = Comment.new
+      @comments = @prototype.comments.includes(:user)
       render "prototypes/show"
     end
   end
